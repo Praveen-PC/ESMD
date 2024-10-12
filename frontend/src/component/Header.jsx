887e0810a1afabf73c4e33137fb32817efb44e81
praveenchakravarthy28@gmail.com <praveenchakravarthy28@gmail.com>
2024-10-12 11:40:08 +0530

    completed

--- frontend/src/component/Header.jsx
+++ frontend/src/component/Header.jsx
@@ -7,8 +7,8 @@ const Header = () => {
     <>
       <nav className="navbar navbar-light bg-light">
         <div className="container-fluid d-flex justify-content-between p-2">
-          <Link to='/' class="navbar-brand mb-0 h1  fw-bold">EMPLOYEE MANAGEMENT</Link>
-          <Link to='/tableview' className="fw-bold text-decoration-none">view</Link>
+          <Link to='/' class="navbar-brand mb-0 h1  fw-bold fs-3 ">ESMD</Link>
+          <Link to='/tableview' className="fw-bold text-decoration-none btn btn-primary">view</Link>
         </div>
       </nav>
     </>
